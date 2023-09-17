554b767f22f82e90bcbadbe05bee01700fbb6fe3
Don Dang <105334355+dondangp@users.noreply.github.com>
2023-09-17 11:01:54 -0500

    implemented tips for each stock

--- fintech.py
+++ fintech.py
@@ -8,8 +8,21 @@ import yfinance as yf
 import plotly.express as px
 from alpha_vantage.fundamentaldata import FundamentalData
 from stocknews import StockNews
-
-
+import random
+TIPS_BEFORE_BUYING = [
+    "Always do your own research and due diligence before buying a stock.",
+    "Diversify your portfolio to spread risk.",
+    "Invest in companies you understand and believe in for the long term.",
+    "Avoid making decisions based solely on price movements or market speculation.",
+    "Monitor your investments regularly and stay updated with market news.",
+    "Consider the company's fundamentals, such as earnings, valuation, and growth potential.",
+    "Always be cautious of stocks with extremely high valuations or rapid price increases.",
+    "Set a budget and avoid investing money you can't afford to lose.",
+    "Consider setting stop-loss orders to limit potential losses.",
+    "Stay patient and avoid emotional decision-making."
+]
+random.shuffle(TIPS_BEFORE_BUYING)
+selected_tips = TIPS_BEFORE_BUYING[:3] 
 # Load environment variables
 load_dotenv()
 
@@ -32,10 +45,13 @@ fig.update_traces(line=dict(color='red'))
 st.plotly_chart(fig)
 
 # Tabs
-pricing_data, fundamental_data, news, stock_comparison = st.tabs(
-    ["Pricing Data", "Fundamental Data", "Trending News", "Stock Comparison"]
+pricing_data, fundamental_data, news, stock_comparison, tips_tab = st.tabs(
+    ["Pricing Data", "Fundamental Data", "Trending News", "Stock Comparison", "Tips"]
 )
-
+with tips_tab:
+    st.write("Consider the following tips before investing in stocks:")
+    for tip in selected_tips:
+        st.write(f"- {tip}")
 # Pricing Data tab
 with pricing_data:
     st.header('Price Movements')
